4b5c2f1c95bb3e14ccc259bb8320447ec6a14fff
Konata Izumi <psasawat@umich.edu>
2018-07-27 00:28:49 -0700

    index slicing seems to be in working state
    I'm tired. I'll think about what gcc turns this into later

--- kirito.h
+++ kirito.h
@@ -34,30 +34,106 @@ struct Index {
 	const B b;
 	const C c;
 
-	Index(A a, B b, C c) : a(a), b(b), c(c) {}
+	constexpr Index(A a, B b, C c) : a(a), b(b), c(c) {}
 };
 
 template <typename A, typename B>
 struct IndexPartial : public Index<A, B, OpenSlot> {
-	IndexPartial(A a, B b) : Index<A, B, OpenSlot>(a, b, {}) {}
+	constexpr IndexPartial(A a, B b) : Index<A, B, OpenSlot>(a, b, {}) {}
 
 	template <typename C, typename = enable_if_t<is_integral<C>::value>>
-	Index<A, B, C> operator()(C c) { return {Index<A, B, OpenSlot>::a, Index<A, B, OpenSlot>::b, c}; }
+	constexpr Index<A, B, C> operator()(C c) const {
+		return {Index<A, B, OpenSlot>::a, Index<A, B, OpenSlot>::b, c};
+	}
 };
 
 template <typename A>
 struct IndexStart : public Index<A, OpenSlot, OpenSlot> {
-	IndexStart(A a) : Index<A, OpenSlot, OpenSlot>(a, {}, {}) {}
+	constexpr IndexStart(A a) : Index<A, OpenSlot, OpenSlot>(a, {}, {}) {}
 
 	template <typename B, typename = enable_if_t<is_integral<B>::value>>
-	IndexPartial<A, B> operator()(B b) { return {Index<A, OpenSlot, OpenSlot>::a, b}; }
+	constexpr IndexPartial<A, B> operator()(B b) const {
+		return {Index<A, OpenSlot, OpenSlot>::a, b};
+	}
 
-	IndexPartial<A, EmptySlot> operator()() { return {Index<A, OpenSlot, OpenSlot>::a, {}}; }
+	constexpr IndexPartial<A, EmptySlot> operator()() const {
+		return {Index<A, OpenSlot, OpenSlot>::a, {}};
+	}
 };
 
-IndexStart<EmptySlot> IndexBuilder() { return {{}}; }
+constexpr IndexStart<EmptySlot> IndexBuilder() { return {{}}; }
 
 template <typename A, typename = enable_if_t<is_integral<A>::value>>
-IndexStart<A> IndexBuilder(A a) { return {a}; }
+constexpr IndexStart<A> IndexBuilder(A a) { return {a}; }
+
+template <
+	typename BaseContainer,
+	typename T = typename BaseContainer::value_type
+>
+class FancyContainer : public BaseContainer {
+	using typename BaseContainer::iterator;
+	using BaseContainer::begin;
+	using BaseContainer::end;
+
+	FancyContainer(const BaseContainer &b) : BaseContainer(b) {}
+
+	using BaseContainer::BaseContainer;
+
+private:
+	template <typename Step>
+	FancyContainer copySlice(iterator start, iterator stop, Step step) {
+		BaseContainer rv;
+		if (start < stop) {
+			for (auto iter = start; iter < stop; iter += step) {
+				rv.push_back(*iter);
+			}
+		} else if (start > stop) {
+			for (auto iter = start; iter > stop; iter += step) {
+				rv.push_back(*iter);
+			}
+		}
+		return rv;
+	}
+
+	template <typename Offset>
+	iterator getStart(Offset o) {
+		if constexpr (is_same<Offset, EmptySlot>::value) {
+			return begin();
+		} else {
+			if (o < 0) {
+				return end() - o;
+			} else {
+				return begin() + o;
+			}
+		}
+	}
+
+	template <typename Offset>
+	iterator getStop(Offset o) {
+		if constexpr (
+			is_same<Offset, EmptySlot>::value ||
+			is_same<Offset, OpenSlot>::value
+		) {
+			return end();
+		} else {
+			if (o < 0) {
+				return end() - o;
+			} else {
+				return begin() + o;
+			}
+		}
+	}
+
+public:
+	template <typename A, typename B, typename C>
+	FancyContainer operator[](Index<A, B, C> ndx) {
+		if constexpr (is_same<C, OpenSlot>::value) {
+			return copySlice(getStart(ndx.a), getStop(ndx.b), 1);
+		} else {
+			return copySlice(getStart(ndx.a), getStop(ndx.b), ndx.c);
+		}
+	}
+};
+
 }  // namespace kirito
 #endif  // KIRITO_H_
--- test.cc
+++ test.cc
@@ -1,12 +1,25 @@
-#include <cassert>
 #include <iostream>
+#include <string>
+#include <vector>
 
+#define DEBUG
 #include "kirito.h"
 
-using kirito::vector;
-using I = kirito::Index;
+#define I kirito::IndexBuilder
 
-void printhelper(vector<int> &v) {
+using std::cout;
+using std::endl;
+using std::string;
+
+constexpr auto N = I()()(3);
+
+template <typename A, typename B, typename C>
+void fn(kirito::Index<A, B, C> i) {
+	cout << i.a << i.b << i.c << endl;
+}
+
+void printhelper(std::vector<int> &v) {
+	std::cout << "YO ";
 	for (auto a : v) {
 		std::cout << a << " ";
 	}
@@ -14,8 +27,20 @@ void printhelper(vector<int> &v) {
 }
 
 int main(int argc, char **argv) {
-	vector<int> a = {1, 2, 3, 4, 5, 6, 7};
+	fn(I(1)(2)(3));
+	fn(I()()(1));
+	fn(I(1)()(3));
+	fn(I()(2)(3));
+	fn(I(1)());
+	fn(I()(2));
+	for (int i = -3; i < 3; ++i) {
+		fn(I(i)());
+	}
+
+	kirito::FancyContainer<std::vector<int>> a = {1, 2, 3, 4, 5, 6, 7};
+
 	auto b = a[I()];
+	fn(I());
 	printhelper(b);
 	auto c = a[I(1)()];
 	printhelper(c);
